ca019a41535f84f172cab4df11f47908a680d100
Bori888 <ilyes.bori8@gmail.com>
2025-12-12 12:41:01 +0100

    api vegpontok

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,27 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\IngatlanController;
+use App\Http\Controllers\KategoriaController;
 
-Route::get('/user', function (Request $request) {
+
+
+// User (példa, ha használod Sanctum auth-ot)
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
-})->middleware('auth:sanctum');
+});
+
+
+// Ingatlan végpontok
+Route::get('/ingatlanok', [IngatlanController::class, 'index']);
+Route::post('/ingatlan', [IngatlanController::class, 'store']);
+Route::get('/ingatlan/{id}', [IngatlanController::class, 'show']);
+Route::put('/ingatlan/{id}', [IngatlanController::class, 'update']);
+Route::delete('/ingatlan/{id}', [IngatlanController::class, 'destroy']);
+
+// Kategoria végpontok
+Route::get('/kategoriak', [KategoriaController::class, 'index']);
+Route::post('/kategoria', [KategoriaController::class, 'store']);
+Route::get('/kategoria/{id}', [KategoriaController::class, 'show']);
+Route::put('/kategoria/{id}', [KategoriaController::class, 'update']);
+Route::delete('/kategoria/{id}', [KategoriaController::class, 'destroy']);
